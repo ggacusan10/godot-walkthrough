6de58615c0ffe662d9f0de179828de47a042538a
ggacusan10 <gvgacusan310@gmail.com>
2025-03-30 19:16:09 -0500

    background / slime / platforms / new world added

--- 2d-platformer-game/scripts/coin.gd
+++ 2d-platformer-game/scripts/coin.gd
@@ -1,1 +1,8 @@
 extends Area2D
+
+
+
+func _on_body_entered(body: Node2D) -> void:
+	print("+1 coin")
+#	# remove coin scene
+	queue_free()
--- 2d-platformer-game/scripts/player.gd
+++ 2d-platformer-game/scripts/player.gd
@@ -1,9 +1,25 @@
 extends CharacterBody2D
 
-
 const SPEED = 130.0
 const JUMP_VELOCITY = -300.0
 
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+
+func handle_animations(direction: float) -> void:
+	if is_on_floor():
+		if direction == 0:
+			animated_sprite_2d.play("idle")
+		elif direction == -1 or direction == 1:
+			animated_sprite_2d.flip_h = (direction == -1)
+			animated_sprite_2d.play("run")
+	else:
+		animated_sprite_2d.play("jump")
+
+func handle_movement(direction: float) -> void:
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -11,15 +27,15 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+	var direction := Input.get_axis("move_left", "move_right")
+
+	handle_animations(direction)
+	
+	handle_movement(direction)
 
 	move_and_slide()

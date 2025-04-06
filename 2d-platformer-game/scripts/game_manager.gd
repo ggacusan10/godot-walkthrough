267141269e6c5f1df616c242915391dfe1a578b8
ggacusan10 <gvgacusan310@gmail.com>
2025-04-06 16:58:00 -0500

    score label

--- 2d-platformer-game/scripts/game_manager.gd
+++ 2d-platformer-game/scripts/game_manager.gd
@@ -1,13 +1,13 @@
 extends Node
 
-@onready var score_label: Label = $ScoreLabel
+@onready var scoreLabel: Label = $CanvasLayer/ScoreLabel
 
 var score = 0
 
 func add_point():
 	score += 1
 	print("Current score: ", score)
-	score_label.text = "Score: " + str(score)
+	scoreLabel.text = "Score: " + str(score)
 	
 func _on_grabbed_fruit() -> void:
 	add_point()

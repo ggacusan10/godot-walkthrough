5149403e81189fbbcfd45b6d1361dc63d33c1683
ggacusan10 <gvgacusan310@gmail.com>
2025-04-07 18:49:16 -0500

    added main menu

---
+++ 2d-platformer-game/scripts/main_menu.gd
@@ -0,0 +1,15 @@
+extends Control
+
+
+
+
+func _on_start_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/game.tscn")
+	
+
+func _on_how_to_play_button_pressed() -> void:
+	print("How to play pressed")
+	
+
+func _on_exit_button_pressed() -> void:
+	get_tree().quit()

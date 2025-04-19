ba28676fa8dcc1cee164b7853bf00afb365940af
ggacusan10 <gvgacusan310@gmail.com>
2025-04-19 17:24:57 -0500

    added How to Play panel

--- 2d-platformer-game/scripts/nodes/ui/main_menu.gd
+++ 2d-platformer-game/scripts/nodes/ui/main_menu.gd
@@ -1,15 +1,22 @@
 extends Control
 
+@onready var how_to_play_panel: Panel = $HowToPlayPanel
 
-
+func _ready() -> void:
+	how_to_play_panel.z_index = 0
+	how_to_play_panel.visible = false
 
 func _on_start_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/game.tscn")
 	
 
 func _on_how_to_play_button_pressed() -> void:
-	print("How to play pressed")
-	
+	how_to_play_panel.z_index = 1
+	how_to_play_panel.visible = true
+
+func _on_back_button_pressed() -> void:
+	how_to_play_panel.z_index = 0
+	how_to_play_panel.visible = false
 
 func _on_exit_button_pressed() -> void:
 	get_tree().quit()
